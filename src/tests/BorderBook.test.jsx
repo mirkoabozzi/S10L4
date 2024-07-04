b1dfc5668b048250822fc874c8e4b1fec0a3958c
Mirko Abozzi <mirko.abozzi@gmail.com>
2024-07-04 17:38:25 +0200

    es.6

--- src/tests/BorderBook.test.jsx
+++ src/tests/BorderBook.test.jsx
@@ -12,4 +12,17 @@ describe("Border Book", () => {
 
     expect(card).toHaveStyle({ border: "3px solid blue" });
   });
+
+  it("Border Book remove color", () => {
+    render(<App />);
+    const cards = screen.getAllByTestId("cards");
+
+    const firstCard = cards[0];
+    const secondCard = cards[1];
+
+    fireEvent.click(secondCard);
+
+    expect(firstCard).not.toHaveStyle({ border: "3px solid blue" });
+    expect(secondCard).toHaveStyle({ border: "3px solid blue" });
+  });
 });
